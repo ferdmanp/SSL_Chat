7def7cdb13b7617329866e5fe1abc769ed9737c3
Павел Фердман <p.ferdman@anc.net.ua>
2017-09-20 17:49:34 +0300

    Initial commit

---
+++ SSL_Chat_Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSL_Chat_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
